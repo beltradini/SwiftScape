d72b59d085c897b5dcd1298398d4474bb127fba9
beltradini <alexbeltrn@outlook.com>
2025-01-11 11:26:16 -0600

    Initial Commit

---
+++ SwiftScape/SwiftScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftScapeApp.swift
+//  SwiftScape
+//
+//  Created by Alejandro Beltrán on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
