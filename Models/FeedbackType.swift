3cf33e430e8c6a4e82c4c83e11ad587c41c7c786
beltradini <alexbeltrn@outlook.com>
2025-03-02 14:06:09 -0600

    Summary Changed

---
+++ Models/FeedbackType.swift
@@ -0,0 +1,12 @@
+// 
+// FeedbackType.swift
+//
+// Created by: Alejandro on 02/06/2025.
+//
+
+import Foundation
+
+enum FeedbackType {
+  case success
+  
+}
--- visionOS/InmersiveView.swift
+++ visionOS/InmersiveView.swift
@@ -10,6 +10,9 @@ import ReplayKitContent
 import SwiftUI
 
 struct InmersiveView: View {
+    @State private var currenntScale: CGFloat = 1.0
+    @State private var currentRotation: Angle = .zero
+    
   var body: some View {
     RealityView { content in 
       let entity = try! Entity.load(named: "3DModel.usdz")}
